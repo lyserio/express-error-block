c2a10406cff369fddaa0277fde9ae805fe0f1c3e
Vince Lwt <vince@lyser.io>
2018-12-24 19:19:05 +0100

    Update name, add Telegram alerts, css customization and background change

--- README.md
+++ README.md
@@ -1,23 +1,54 @@
-# express-mini-errors
+# express-error-page
 
-A simple error handler for Express (4+). 
+A minimalsit simple error handler for Express (4+) which returns an error page and can send you bug alerts via Telegram.
 
 Support error reporting via `next()` and automatically handles 404.
 
-It renders a simple HTML page. 
+It renders a simple HTML page, which can be customised via options.
 
-## Future features
-- Telegram alerts
-- Customization of background
+## Who uses it?
+
+<table>
+<tr>
+<td align="center"><a href="https://nucleus.sh/"><img src="https://nucleus.sh/logo_color.svg" height="64" /></a></td>
+</tr>
+<tr>
+<td align="center">Nucleus</td>
+</tr>
+</table>
+
+_👋 Want to be listed there? [Contact me](mailto:vince@lyser.io)._
+
+
+## Features
+- Telegram alerts on bugs
+- Change the page background
+- Add custom CSS
+- Respond in JSON if API detected
 
 ## How to use
 
 ```
-npm install express-mini-errors
+npm install express-error-page
 ```
 
 Add to the end of your app:
 ```
-app.use(require('express-mini-errors))
+const errorsHandler = require('express-error-page')
+
+app.use(errorsHandler({
+	background: 'red', // Can be any valid CSS background property value
+	customCSS: 'body {color: #000}', 
+	telegramAlerts: false,
+	telegramToken: 'xxxxxx',
+	telegramConversation: 'xxxxx'
+}))
 ```
+
+
+## Telegram Alerts
+
+You can receive alerts via Telegram when an unhandled error happen in your app.
+
+Get a token as explained [here](https://www.siteguarding.com/en/how-to-get-telegram-bot-api-token) and get your user id as explained [here]()
 
--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
+let background = '#2196F3'
+
+let TelegramBot = null
+let telegramBot = null
+let telegramAlerts = false
+let telegramToken = null
+let telegramConv = null
+let customCSS = ''
+
 const errorHTML = (title, message) => {
 	return `
 	<style>
 		body {
-			background: lightblue;
-			font-family: sans-serif;
+			background: ${background};
+			font-family: Lato, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
 			display: flex;
 			justify-content: center;
 			flex-direction: column;
 			text-align: center;
+			color: #fff;
 		}
+
+		h1, h2 {
+			font-weight: 200;
+		}
+
+		${customCSS}
 	</style>
 	<body>
 		<div>
@@ -27,26 +43,48 @@ const errorHTML = (title, message) => {
 router.use((err, req, res, next) => {
 	console.error(err)
 
-	if (app.get('env') === 'development') {
+	if (!process.env.NODE_ENV) { // https://stackoverflow.com/questions/34227216/process-env-vs-app-getenv-on-getting-the-express-js-environment
 		var message = err
 	} else {
 		var message = (typeof err === 'string' ? err : 'Internal server error')
+	}
+
+	if (telegramAlerts) {
+		telegramBot.sendMessage(telegramConversation, `Hi,\n\nAn error happened in your app.\n\n${message}`);
 	}
 	
 	if (req.xhr || req.accepts('json', 'html') === 'json') {
 		res.status(500).json({ error: message })
 	} else {
-		res.status(500).send(errorHTML("Oops! Looks like something went wrong here.", message))
+		res.status(500).send(errorHTML("Mmmh, something went wrong appened here.", message))
 	}
 })
 
-
+// 404 Handler (last route)
 router.use((req, res, next) => {
 	if (req.xhr || req.accepts('json', 'html') === 'json') {
 		res.status(404).json({ error: 'not found' })
 	} else {
-		res.status(404).send(errorHTML("Sorry, can't find that!"))
+		res.status(404).send(errorHTML("Looks like you got lost!", "404 - page not found"))
 	}
 })
 
-module.exports = router
+module.exports = (options) => {
+	
+	if (options.background) background = options.background
+	if (options.customCSS) customCSS = options.customCSS
+
+	if (options.telegramAlerts) {
+		options.telegramAlerts = true
+	
+		if (!TelegramBot) TelegramBot = require('node-telegram-bot-api')
+	
+		telegramToken = options.telegramToken
+	
+		telegramBot = new TelegramBot(telegramToken, {polling: false})
+	
+		telegramConv = options.telegramConversation
+	}
+
+	return router
+}
